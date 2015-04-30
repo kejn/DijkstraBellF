dac4681597d359a0213452600313ee5f441b5e0a
kejn <kaen04@gmail.com>
2015-04-30 08:52:27 +0200

    Create LKrawedzi.h

---
+++ LKrawedzi.h
@@ -0,0 +1,26 @@
+/*******************************************
+ * Written in 2014 by Kamil Niemczyk
+ *
+ * PL:
+ * ! Kod kopiować wraz z tym komentarzem !
+ * W przypadku wprowadzenia zmian
+ * dopisać się do listy autorów kodu.
+ * Rozprowadzać bezpłatnie.
+ * Wyrażam zgodę na używanie programu oraz
+ * jego kodu w celach dydaktycznych.
+ *
+ * EN:
+ * ! Copy only with this comment !
+ * In case of any changes add your nick/name
+ * to authors list.
+ * Distribute free of charge.
+ * I agree to use this program and its code
+ * for educational purposes.
+ *******************************************/
+#ifndef LKRAWEDZI_HH
+#define LKRAWEDZI_HH
+
+#include "Edge.hh"
+#include <list>
+
+#endif // LKRAWEDZI_HH
